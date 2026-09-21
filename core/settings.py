cfdf03ad622f737746a16a4a2c23449083f9f45f
tukubaevhadisbek <tukubaevhadisbek@gmail.com>
2026-09-21 18:10:12 +0600

    Add employer application status management3

--- core/settings.py
+++ core/settings.py
@@ -120,10 +120,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
